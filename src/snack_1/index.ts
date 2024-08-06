44669808ae2f8cbacd3094e39860529fdcb8b9f7
Denis-Turbatu <turbatudenis81@gmail.com>
2024-08-06 16:10:25 +0200

    first commit

---
+++ src/snack_1/index.ts
@@ -0,0 +1,2 @@
+// 1. Write a TypeScript program that declares a variable `name` and assigns it a string value. Also declare a variable `age` and assign it a number value. Finally print the values of name and age.
+
