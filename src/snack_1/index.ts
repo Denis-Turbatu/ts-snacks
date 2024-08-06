9d8d71ab9f1e9401a75db0c6ce034fa1761f4094
Denis-Turbatu <turbatudenis81@gmail.com>
2024-08-06 16:57:58 +0200

    feat: snack_1 with import of readline_sync

--- src/snack_1/index.ts
+++ src/snack_1/index.ts
@@ -1,2 +1,10 @@
 // 1. Write a TypeScript program that declares a variable `name` and assigns it a string value. Also declare a variable `age` and assign it a number value. Finally print the values of name and age.
 
+import * as readlineSync from 'readline-sync';
+
+
+let myName: string = readlineSync.question("Inserisci il nome: ");
+let myAge: string = readlineSync.question("Inserisci la tua eta: ");
+
+console.log(`Ciao, ${myName}! Hai ${myAge} anni`);
+console.log(typeof myName, typeof myAge);
